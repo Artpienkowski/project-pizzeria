b7b26fc9bf7a6b49e04910283e8d850c65f6c2b0
Artur Pieńkowski <artpienkowski@gmail.com>
2022-07-10 15:29:21 +0200

    finish processOrder method

--- src/js/script.js
+++ src/js/script.js
@@ -143,8 +143,23 @@
         // for every option in this category
         for(let optionId in param.options) {
         // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
-        const option = param.options[optionId];
-        console.log(optionId, option);
+          const option = param.options[optionId];
+          console.log(optionId, option);
+
+          // check if there is param with a name of paramId in formData and if it includes optionId
+          if(formData[paramId] && formData[paramId].includes(optionId)) {
+            // check if the option is not default
+            if(!option.default == true) {
+              // add option price to price variable
+              price = option.price + price
+            }
+          } else {
+            // check if the option is default
+            if(option.default == true) {
+              // reduce price variable
+              price = price - option.price
+            }
+          }
         }
       }
       // update calculated price in the HTML
